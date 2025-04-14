95055797441bf4c40c56000bd360bd0ce65fbfea
Vaibhav Yesalwad <yesalwadvaibhav@gmail.com>
2025-04-14 15:54:15 +0530

    Add additional loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="prose">
+      <Skeleton />
+      <Skeleton height="25rem" />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-import { TextField, Button, Callout, Text } from "@radix-ui/themes";
-import "easymde/dist/easymde.min.css";
-import { Controller, useForm } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchema";
-import { z } from "zod";
-import errorsToRecord from "@hookform/resolvers/io-ts/dist/errorsToRecord.js";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import delay from "delay";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
@@ -41,6 +41,8 @@ const NewIssuePage = () => {
       setError("An unexpected error happened.");
     }
   });
+
+  delay(2000);
 
   return (
     <div className="max-w-xl">
