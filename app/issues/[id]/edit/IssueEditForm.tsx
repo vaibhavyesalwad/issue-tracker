0f36b0ed45a2aacc8f9378ae4ffa956ab6905856
Vaibhav Yesalwad <yesalwadvaibhav@gmail.com>
2025-04-17 14:28:18 +0530

    Handle Errors

--- app/issues/[id]/edit/IssueEditForm.tsx
+++ app/issues/[id]/edit/IssueEditForm.tsx
@@ -1,7 +1,7 @@
 "use client";
+import { Issue } from "@/app/generated/prisma";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
-import { Issue } from "@/app/generated/prisma";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
