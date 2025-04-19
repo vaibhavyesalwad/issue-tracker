99a0befb9eb9adddab2ea8e18362904fb40b1084
Vaibhav Yesalwad <yesalwadvaibhav@gmail.com>
2025-04-19 18:48:05 +0530

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,10 +1,10 @@
 import { IssueStatusBadge, Link } from "@/app/components";
-import NextLink from "next/link";
-import { prisma } from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
-import IssueActions from "./IssueActions";
 import { Issue, Status } from "@/app/generated/prisma";
+import { prisma } from "@/prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import NextLink from "next/link";
+import IssueActions from "./IssueActions";
 
 interface Props {
   searchParams: { status: Status; orderBy: keyof Issue };
@@ -13,16 +13,27 @@ interface Props {
 const IssuesPage = async ({ searchParams }: Props) => {
   searchParams = await searchParams;
   const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-  const issues = await prisma.issue.findMany({ where: { status } });
 
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy,
+  });
 
   return (
     <div>
