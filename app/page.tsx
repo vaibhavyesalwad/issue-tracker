d6e96fb63275af30621dcdc6f694ba4e4ceebe13
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2025-10-25 14:56:08 +0530

    Refactor: Dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,6 @@ export default async function Home({
   searchParams: { page: string };
 }) {
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt((await searchParams).page) || 1}
-    ></Pagination>
+    <></>
   );
 }
